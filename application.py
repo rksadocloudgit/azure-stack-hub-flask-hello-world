9161a7b871fca1d22707fd7b191f363b4cadd458
rksadocloudgit <56008512+rksadocloudgit@users.noreply.github.com>
2021-02-05 08:34:17 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Updated World!"
+    return "Hello Updated World! = this is for test1"
 
 if __name__ == '__main__':
     app.run()
